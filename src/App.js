bf4a82aff80cfa4ce7b6964f048c33e1cf084fca
yeffasol <yeffasol@mail.com>
2018-08-03 14:06:41 +0300

    Add counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import Counter from '../src/components/Counter/Counter';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+	    <Counter />
     );
   }
 }
